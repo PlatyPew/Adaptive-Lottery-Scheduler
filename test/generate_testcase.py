d3a3568342a8c59ed44f5a6a17dd9c3fac5fef90
Platy <daryl.limkz@gmail.com>
2022-03-19 10:08:33 +0800

    feat(generator): made program more customisable
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- test/generate_testcase.py
+++ test/generate_testcase.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python3
 from random import randint
+import argparse
 
-START_FILE = 9
+parser = argparse.ArgumentParser(description='Generate test cases')
+parser.add_argument('-p', type=int, help='Minimum processes', default=30)
+parser.add_argument('-P', type=int, help='Maximum processes', default=50)
+
+parser.add_argument('-a', type=int, help='Minimum arrival time', default=0)
+parser.add_argument('-A', type=int, help='Maximum arrival time', default=1000)
+
+parser.add_argument('-b', type=int, help='Minimum burst time', default=1)
+parser.add_argument('-B', type=int, help='Maximum burst time', default=1000)
+
+args = parser.parse_args()
+
+START_FILE = 1
 END_FILE = 30
 
-MIN_PROCESSES = 1
-MAX_PROCESSES = 50
+MIN_PROCESSES = args.p
+MAX_PROCESSES = args.P
 
-MIN_AT = 0
-MAX_AT = 50
+MIN_AT = args.a
+MAX_AT = args.A
 
-MIN_BT = 1
-MAX_BT = 50
+MIN_BT = args.b
+MAX_BT = args.B
 
 
 def gen_processes(num_of_proceeses):
@@ -28,5 +41,5 @@ if __name__ == '__main__':
     for i in range(START_FILE, END_FILE + 1):
         processes = gen_processes(randint(MIN_PROCESSES, MAX_PROCESSES))
 
-        with open(f'testcase{i}.txt', 'w') as f:
+        with open(f'testcase{i:03d}.txt', 'w') as f:
             f.write(processes)
